bd8fd14c5069322fb0143c9e996768ea5696b28e
ZhouXianghui <728678732@qq.com>
2022-03-01 10:51:33 +0800

    add readme 阅读记录

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import "./App.css";
 import React from "react";
 // import { PropsComponentDemo, FatherIndex } from "./components/basic/4_props";
 // import FormDemo from "./components/basic/4_props/form";
-import { LifeCycleF, LifeCycleC } from "./components/basic/5_lifeCycle";
+// import { LifeCycleF, LifeCycleC } from "./components/basic/5_lifeCycle";
+import {
+  FuncComponent,
+  FunctionLifeCycleContainer,
+} from "./components/basic/5_lifeCycle";
 
 function App() {
   return (
@@ -10,8 +14,10 @@ function App() {
       {/* <PropsComponentDemo />
       <FatherIndex />
       <FormDemo /> */}
-      <LifeCycleF name="hello fcuntion" />
-      <LifeCycleC name="hello class" />
+      {/* <LifeCycleF name="hello fcuntion" />
+      <LifeCycleC name="hello class" /> */}
+      <FuncComponent />
+      <FunctionLifeCycleContainer />
     </div>
   );
 }
--- src/components/basic/5_lifeCycle/index.jsx
+++ src/components/basic/5_lifeCycle/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useLayoutEffect, useState } from "react";
 import PropTypes from "prop-types";
 
 export function LifeCycleF({ name }) {
@@ -7,7 +7,7 @@ export function LifeCycleF({ name }) {
 
 LifeCycleF.propTypes = {
   name: PropTypes.string,
-};  
+};
 
 export class LifeCycleC extends Component {
   static propTypes = {
@@ -24,7 +24,7 @@ export class LifeCycleC extends Component {
     console.log(`constructor: `, props);
   }
 
-  // 2.
+  // 2. 存在 getDerivedStateFromProps 则不执行 componentWillMount
   static getDerivedStateFromProps(nextProps, preState) {
     console.log("getDerivedStateFromProps preState:", preState);
     console.log("getDerivedStateFromProps nextProps:", nextProps);
@@ -55,13 +55,17 @@ export class LifeCycleC extends Component {
 
   // ===== mountClassInstance end =====
 
+  // commit 阶段
+  // componentDidMount 针对初始化
   componentDidMount() {
     console.log("componentDidMount");
   }
 
-  componentDidUpdate(preProps, preState) {
+  // componentDidUpdate 针对更新
+  componentDidUpdate(preProps, preState, snapshot) {
     console.log("componentDidUpdate preProps: ", preProps);
     console.log("componentDidUpdate preState: ", preState);
+    console.log("componentDidUpdate snapshot: ", snapshot);
   }
 
   // stop working in react 17
@@ -70,15 +74,133 @@ export class LifeCycleC extends Component {
   //   console.log("UNSAFE_componentWillReceiveProps nextContext: ", nextContext);
   // }
 
-  shouldComponentUpdate(nextProps, nextState) {
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
     console.log("shouldComponentUpdate nextProps: ", nextProps);
     console.log("shouldComponentUpdate nextState: ", nextState);
+    console.log("shouldComponentUpdate nextContext: ", nextContext);
     return true;
   }
 
+  UNSAFE_componentWillUpdate() {}
+
+  getSnapshotBeforeUpdate(prevProps, preState) {
+    return {
+      ...prevProps,
+      ...preState,
+    };
+  }
+
+  // 更新阶段
+  UNSAFE_componentWillReceiveProps() {}
 }
 
 // React 两个重要阶段
 // 1. render 调和阶段
 //    遍历 React fiber 树，目的是发现不同 diff
 // 2. commit
+
+// 函数组件生命周期替代
+export function FuncComponent(props) {
+  const [count, setCount] = useState(0);
+  const [x, setX] = useState(0);
+  const [color, setColor] = useState("red");
+  useEffect(() => {
+    console.log("useEffect");
+  });
+  useLayoutEffect(() => {
+    console.log("useLayoutEffect");
+  });
+
+  // componentDidMount 替代方案
+  useEffect(() => {
+    // 请求数据 事件监听 操纵dom
+    console.log("useEffect 模拟 componentDidMount");
+  }, []);
+
+  // componentWillUnmount 替代方案
+  useEffect(() => {
+    // 请求数据 事件监听 操纵dom 添加定时器、掩饰其
+    return function componentWillUnmount() {
+      // 解除事件监听 清楚定时器、延时器
+    };
+  }, []); // dep=[]
+
+  // componentWillReceiveProps 代替方案
+  useEffect(() => {
+    console.log("props变化: componentWillReceiveProps");
+  }, [props]);
+  useEffect(() => {
+    console.log("props.number变化: componentWillReceiveProps");
+  }, [props.number]);
+
+  // componentDidUpdate 替代方案
+  useEffect(() => {
+    console.log("组件更新完成: componentDidUpdate");
+  }); // 没有 dep 依赖项
+
+  return (
+    <div>
+      <h1>this is function component</h1>
+      <h2 style={{ color: color }}>count: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>increment</button>
+      <button onClick={() => setX(x + 1)}>change x</button>
+      <button onClick={() => setColor("yellow")}>change color</button>
+    </div>
+  );
+}
+
+FuncComponent.propTypes = {
+  number: PropTypes.number,
+};
+
+function FunctionLifeCycle(props) {
+  const [num, setNum] = useState(0);
+
+  // 模拟 componentDidMount
+  useEffect(() => {
+    /* 请求数据 ， 事件监听 ， 操纵dom  ， 增加定时器 ， 延时器 */
+    console.log("组件挂载完成: componentDidMount");
+
+    // 模拟 componentWillUnmount
+    return function componentWillUnmount() {
+      /* 解除事件监听器 ，清除 */
+      console.log("组件销毁: componentWillUnmount");
+    };
+  }, []); /* 切记 dep = [] */
+
+  useEffect(() => {
+    console.log("props变化: componentWillReceiveProps");
+  }, [props]);
+
+  useEffect(() => {
+    /*  */
+    console.log(" 组件更新完成: componentDidUpdate ");
+  });
+
+  return (
+    <div>
+      <div> props : {props.number} </div>
+      <div> states : {num} </div>
+      <button onClick={() => setNum((state) => state + 1)}>改变state</button>
+    </div>
+  );
+}
+
+FunctionLifeCycle.propTypes = {
+  number: PropTypes.number,
+};
+
+export function FunctionLifeCycleContainer() {
+  const [number, setNumber] = React.useState(0);
+  const [isRender, setRender] = React.useState(true);
+  return (
+    <div>
+      {isRender && <FunctionLifeCycle number={number} />}
+      <button onClick={() => setNumber((state) => state + 1)}>改变props</button>
+      <br />
+      <button onClick={() => setRender(true)}>添加组件</button>
+      <br />
+      <button onClick={() => setRender(false)}>卸载组件</button>
+    </div>
+  );
+}
